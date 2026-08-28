79bf9f24c76817e7991dc10dd3a8adf6aa80c51e
LoganS <logan.sieracki@icloud.com>
2026-08-27 20:04:29 -0400

    hry

--- playwright.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from '@playwright/test';
-export default defineConfig({
-  testDir: './tests/browser',
-  timeout: 120000,
-  use: { baseURL: 'http://127.0.0.1:4173', headless: true },
-  webServer: { command: 'npm run preview -- --host 127.0.0.1 --port 4173', url: 'http://127.0.0.1:4173', reuseExistingServer: false, timeout: 120000 },
-});
